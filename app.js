b62bce74f0f51d1ae67e9e403e89349ad5cf1910
ryzetech <mr.2702.mm@gmail.com>
2022-12-01 10:15:48 +0100

    🚧 misc

--- app.js
+++ app.js
@@ -8,15 +8,33 @@ const { Server } = require("node-ws-packets");
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+const { Operation, OpBatch } = require('./src/packets.js');
+
 app.use(express.static(path.join(__dirname, 'public')));
 // app.set("view engine", "ejs");
+
 app.listen(config.hostport, () => {
   console.log(`Listening on port ${config.hostport}`);
 });
 
+// open server and manager
 const monitorsrv = new WebSocket.Server({ port: config.wsport });
 const msm = new Server(monitorsrv, { log: true });
+msm.addPacket(new Operation());
 
-msm.onConnect((client) => {
+msm.onConnect(async (client) => {
+  let ops = await prisma.operation.findMany({
+    where: { completed: false }
+  });
+  await client.sendPacket(new OpBatch(ops));
+});
 
+// prisma middleware: catch create and update calls to broadcast those changes
+prisma.$use(async (params, next) => {
+  console.log(params);
+  const result = await next(params);
+  if (params.model === 'Operation' && ["create", "update"].includes(params.action)) {
+    msm.broadcast(new Operation(result));
+  }
+  return result;
 });
