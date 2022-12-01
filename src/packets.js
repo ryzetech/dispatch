f5fb2855b88665a77fe1071f05f5bcda8f0fee21
ryzetech <mr.2702.mm@gmail.com>
2022-12-01 10:14:15 +0100

    🏷️ updated operation and added opbatch

--- src/packets.js
+++ src/packets.js
@@ -5,13 +5,22 @@ class Operation extends Packet {
   constructor(payload) {
     const model = {
       id: String,
-      mtscode: new DetailedValue(Number, { required: false }),
+      mtscode: new DetailedValue(Number, { required: false, min: 1, max: 5 }),
       place: String,
       report: String,
-      createdAt: String,
+      createdAt: Date,
     }
     super("Operation", payload, model);
   }
 }
 
-module.exports = { Operation };
+class OpBatch extends Packet {
+  constructor(array) {
+    const model = {
+      ops: [Operation]
+    }
+    super("OpBatch", {ops: array}, model);
+  }
+}
+
+module.exports = { Operation, OpBatch };
